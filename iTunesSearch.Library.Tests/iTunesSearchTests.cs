caf8a7ed86196a02238a50b318ea7e9a3f055edd
danesparza <esparza.dan@gmail.com>
2014-07-29 17:05:15 -0400

    Added a lot more fields to episode model

--- iTunesSearch.Library.Tests/iTunesSearchTests.cs
+++ iTunesSearch.Library.Tests/iTunesSearchTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
+using iTunesSearch.Library.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace iTunesSearch.Library.Tests
@@ -19,7 +21,33 @@ namespace iTunesSearch.Library.Tests
 
             //  Assert
             Assert.IsTrue(items.Episodes.Any());
+        }
 
+        [TestMethod]
+        public void GetSeasonsAndEpisodesForShow_ValidShow_ReturnsEpisodes()
+        {
+            //  Arrange
+            iTunesSearchManager search = new iTunesSearchManager();
+            string showName = "Modern Family";
+
+            //  Act
+            var items = search.GetEpisodesForShow(showName, 200).Result;
+            var seasons = from episode in items.Episodes
+                          orderby episode.Number
+                             group episode by episode.SeasonNumber into seasonGroup
+                             orderby seasonGroup.Key
+                             select seasonGroup;
+
+            //  Assert
+            foreach(var seasonGroup in seasons)
+            {
+                Debug.WriteLine("Season number: {0}", seasonGroup.Key);
+
+                foreach(TVEpisode episode in seasonGroup)
+                {
+                    Debug.WriteLine("Ep {0}: {1}", episode.Number, episode.Name);
+                }
+            }
         }
     }
 }
